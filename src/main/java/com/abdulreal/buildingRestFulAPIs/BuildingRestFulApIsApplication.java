96ffcd2e2eda28d1948c79670efbaf60c814f715
abdulreal96 <69744932+abdulreal96@users.noreply.github.com>
2023-05-02 14:59:40 +0100

    first commit

---
+++ src/main/java/com/abdulreal/buildingRestFulAPIs/BuildingRestFulApIsApplication.java
@@ -0,0 +1,13 @@
+package com.abdulreal.buildingRestFulAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingRestFulApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingRestFulApIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abdulreal/buildingRestFulAPIs/BuildingRestFulApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulreal.buildingRestFulAPIs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingRestFulApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
